bb06a4ecef1a67e8f9ec84c2e0b3ebb96be8d17d
tochapado <tochapado710@gmail.com>
2025-06-13 23:08:09 -0300

    #05

---
+++ 05_blurry_loading/script.js
@@ -0,0 +1,22 @@
+const bg = document.querySelector('.bg');
+const loadText = document.querySelector('.loading-text');
+
+let load = 0;
+let intervalID = setInterval(blurring, 30);
+
+function blurring()
+{
+    load++;
+    loadText.textContent = load + '%';
+    loadText.style.opacity = (1 - (load / 100));
+    // loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = 'blur(' + (30 - (load * 30 / 100)) + 'px)';
+
+    if(load > 99) clearInterval(intervalID);
+};
+
+// // map numbers function
+// function scale(num, in_min, in_max, out_min, out_max)
+// {
+//     return out_min + ((num - in_min) * (out_max - out_min)) / (in_max - in_min);
+// };
