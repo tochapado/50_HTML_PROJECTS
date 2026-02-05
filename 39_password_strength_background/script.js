529ec5a4742c47ec38fc79f3ea36d9e0b78f91d6
tochapado <tochapado710@gmail.com>
2026-02-05 14:19:31 -0300

    #39

---
+++ 39_password_strength_background/script.js
@@ -0,0 +1,7 @@
+password.addEventListener("input", e =>
+{
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`
+})
